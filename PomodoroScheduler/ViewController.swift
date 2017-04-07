034d0c65e1fdd845cc5cff615519859ddc0bac36
Joshua Zahner <joshuazahner@gmail.com>
2017-04-06 21:03:57 -0500

    Basic Views and Initial Setup

--- PomodoroScheduler/ViewController.swift
+++ PomodoroScheduler/ViewController.swift
@@ -9,11 +9,30 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    @IBOutlet var circularView: NSView!
+    @IBOutlet var timeRemainingLabel: NSTextField!
+    
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm:ss"
+        return formatter
+    }()
+    
+    var timer = Timer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        circularView.layer?.backgroundColor = CGColor(red: 1.0, green: 0, blue: 0, alpha: 1.0)
+        
+        timeRemainingLabel.stringValue = dateFormatter.string(from: Date())
+        
+        timeRemainingLabel.isEditable = false
+        timeRemainingLabel.isBezeled = false
+        timeRemainingLabel.isSelectable = false
+        
     }
 
     override var representedObject: Any? {
